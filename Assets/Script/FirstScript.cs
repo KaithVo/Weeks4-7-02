c851ff2c832439bdfb390385465bfd487331b751
mymacbook <vokar@shernet.sheridancollege.ca>
2026-01-31 22:28:40 -0500

    Tank Spawner

--- Assets/Script/FirstScript.cs
+++ Assets/Script/FirstScript.cs
@@ -1,44 +1,29 @@
+
 using UnityEngine;
+
+
 
 public class FirstScript : MonoBehaviour
 {
     public float speed = 0.01f;
-    Vector2 bottomLeft;
-    Vector2 topRight;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+void Start()
     {
-        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
-        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+// Update is called once per frame
+void Update()
     {
         //move the square
         Vector3 newPosition = transform.position;
-        newPosition.x += speed * Time.deltaTime;
-
-        //check if the Equare is at the edge of the screen
+        newPosition.x += speed;
+        transform.position = newPosition;
+        //check if the square is at the edge of the screen
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
-
-        //test for the left edge
-        if (screenPos.x < 0)
-        {
-            //set our position to be the world space position under pixel 0 in X
-            newPosition.x = bottomLeft.x;
+        if (screenPos.x < 0 || screenPos.x >
+        Screen.width)
             //Y: multiply the speed by -1
             speed = speed * -1;
-            //test for the right edge
-        }
-
-        if (screenPos.x > Screen.width)
-        {
-            //set our position to be the world space position under pixel Screen.width in X
-            newPosition.x = topRight.x;
-            //Y: multiply the speed by -1
-            speed = speed * -1;
-            transform.position = newPosition;
-        }
     }
+
 }
